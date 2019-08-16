58f9f1b790d038e38223ad85bbf1eeb8cc3fe166
garchomp-game <otoka.japanesedrum@gmail.com>
2019-08-16 17:52:48 +0900

    途中コミット

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { HeroService } from './../hero.service';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+  public heroes: Hero[];
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
